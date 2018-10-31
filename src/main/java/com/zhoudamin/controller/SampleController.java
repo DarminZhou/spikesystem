0525a95c5a5ce27706f9098000b075f1b27d98e6
DarminZhou <790471332@qq.com>
2018-11-01 00:29:46 +0800

    update

--- src/main/java/com/zhoudamin/controller/SampleController.java
+++ src/main/java/com/zhoudamin/controller/SampleController.java
@@ -1,6 +1,13 @@
 package com.zhoudamin.controller;
 
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class SampleController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 }
